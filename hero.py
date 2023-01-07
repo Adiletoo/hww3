b96f1c40481516faa5f29e8207ae9bc15d6dc8c8
Adilet <9adilet9@gmail.com>
2023-01-07 23:31:31 +0600

    hw2

--- hero.py
+++ hero.py
@@ -23,7 +23,62 @@ class SuperHero:
 
 
 hero = SuperHero('Saitama', 'Bald cloak', 'Strongest', 999, 'heroism')
-print(hero.name_hero())
-print(hero.extra_health())
-print(hero.__len__())
-print(hero.__str__())
+# print(hero.name_hero())
+# print(hero.extra_health())
+# print(hero.__len__())
+# print(hero.__str__())
+
+
+
+class FriendHero(SuperHero):
+    fly = False
+    damage = False
+    kunai = True
+
+    def extra_health(self):
+        self.health_points **= 2
+        FriendHero.fly = True
+        return self.health_points
+
+    def __str__(self):
+        return f'fly in the True_phrase'
+
+
+friend = FriendHero("King", 'Strongest Man of the planet', 'extreme luck', 50, 'I am the strongest')
+print(friend.extra_health())
+print(friend.fly)
+print(friend.__str__())
+
+
+class BrotherHero(SuperHero):
+    fly = False
+    damage = False
+    sword = True
+
+    def extra_health(self):
+        self.health_points **= 2
+        BrotherHero.fly = True
+        return self.health_points
+
+    def __str__(self):
+        return f'fly in the True_phrase'
+
+
+bro = BrotherHero('Genos', 'Kiborg', 'Flight', 666, 'my teacher is the strongest')
+print(bro.extra_health())
+print(BrotherHero.fly)
+print(bro.__str__())
+
+
+class Villain(BrotherHero):
+    people = 'monster'
+
+    def gen_x(self):
+        pass
+
+    def crit(self):
+        self.damage **= 2
+        return self.damage
+
+
+print(BrotherHero.damage)
